52aad0cc94f3679be456b139f8eb78337ce21dc3
Eva Gonalez Ferreira <evagonf12@gmail.com>
2022-01-02 14:23:24 +0100

    Defined Design System

---
+++ TheMostPopularAPI/TheMostPopularAPI/Design System/Resources/String/Extensions/Texts+Global.swift
@@ -0,0 +1,10 @@
+
+
+import Foundation
+extension Texts {
+    
+    struct Global {
+        static let day = NSLocalizedString("global_day", comment: "")
+        static let days = NSLocalizedString("global_days", comment: "")
+    }
+}
